d4ea431e5cb90b9e65215cb7d313e21e2a937dce
04amanrajj <04.aman.raj@gmail.com>
2024-11-08 23:22:21 +0530

    Backend: Jwt authorization, Token, Admin control

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const dataURL = "http://localhost:3400/data";
+const textBox = document.querySelector(".text-showcase");
+const token = localStorage.getItem("token");
+async function loadData() {
+  try {
+    let response = await fetch(dataURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+    let data = await response.json();
+    console.log(response);
+    textBox.innerHTML = `
+    <pre>${data.data|| "Token expired re-login to recreate token"}</pre>`;
+  } catch (error) {
+    console.log({ error: error.message });
+  }
+}
+loadData();
